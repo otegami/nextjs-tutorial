1b18783ac58739eff50192329afba980b89818f4
otegami <a.s.takuya1026@gmail.com>
2021-07-12 16:13:40 -0700

    Added Head component to manipulate metadata

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>Frist Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">

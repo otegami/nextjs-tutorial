4430566b96ae0d2c9e18657c2dd8839398759347
otegami <a.s.takuya1026@gmail.com>
2021-12-13 09:04:06 +0900

    Implemented home component with blog data

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,19 @@ const Home = ({ allPostsData }) => {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>I'm web developer now. </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => {
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          })}
+        </ul>
       </section>
     </Layout>
   )

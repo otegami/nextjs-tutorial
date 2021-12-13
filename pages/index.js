56b82fa2325d532cdb525465258987a43cd76d1a
otegami <a.s.takuya1026@gmail.com>
2021-12-14 08:25:44 +0900

    Learnt Server Side Rendering And Client Site Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { getSortedPostsData } from '../lib/posts'
+import getSortedPostsData from '../lib/posts'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/Layout'
 import utilStyles from '../styles/utils.module.css'
@@ -21,7 +21,7 @@ const Home = ({ allPostsData }) => {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => {
+          {allPostsData.map(({ id, date, title }) =>
             <li className={utilStyles.listItem} key={id}>
               {title}
               <br />
@@ -29,7 +29,7 @@ const Home = ({ allPostsData }) => {
               <br />
               {date}
             </li>
-          })}
+          )}
         </ul>
       </section>
     </Layout>

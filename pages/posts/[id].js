99c2cbaaabe81e19e43018cc447d3369c0f9c4d3
otegami <a.s.takuya1026@gmail.com>
2021-12-15 08:31:15 +0900

    Changed file name with git mv

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 
 import Head from 'next/head'
 import Layout from '../../components/layout'
-import Date from '../../components/Date'
+import Date from '../../components/date'
 
 import utilStyles from '../../styles/utils.module.css'
 
